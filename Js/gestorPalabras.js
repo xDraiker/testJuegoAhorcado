9429ba10d064b87ac55aec82b44303eec842582e
xDraiker <daniel.vz.gc97@gmail.com>
2022-09-07 20:38:49 -0500

    Pantalla para eliminar palabras y DOM

--- Js/gestorPalabras.js
+++ Js/gestorPalabras.js
@@ -5,6 +5,8 @@ if(!localStorage.getItem('palabras')){
     localStorage.setItem('palabras', JSON.stringify(Palabras));
 }
 let palabrasDB = JSON.parse(localStorage.getItem('palabras'));
+const boxSelect = document.getElementById('configRemoveSelect');
+const fragmentList = document.createDocumentFragment();
 
 const validar = palabra => {
     const regex = /[á-źÁ-Ź]/g;
@@ -20,7 +22,47 @@ const addPalabra = (palabra) =>{
     palabrasDB.arrayPalabras.push(palabra.toUpperCase());
     localStorage.setItem('palabras', JSON.stringify(palabrasDB));
 };
+const createList = () =>{
+    const contentList = document.createElement('div');
+    contentList.setAttribute('id','listSelect');
+    contentList.classList.add('list--select');
+    for(let i =0; i < palabrasDB.arrayPalabras.length; i++){
+        const itemRadio = document.createElement('input');
+        const itemLabel = document.createElement('label');
+
+        itemRadio.setAttribute('type', 'radio');
+        itemRadio.setAttribute('id',`${palabrasDB.arrayPalabras[i]}`);
+        itemRadio.setAttribute('name','palabras');
+        itemRadio.classList.add('select__radio');
+       
+        itemLabel.setAttribute('for',`${palabrasDB.arrayPalabras[i]}`);
+        itemLabel.setAttribute('id',`${palabrasDB.arrayPalabras[i]}${i}`);
+        itemLabel.classList.add('select__item');
+        itemLabel.textContent = `${palabrasDB.arrayPalabras[i]}`;
+
+        fragmentList.appendChild(itemRadio);
+        fragmentList.appendChild(itemLabel);
+    }
+    contentList.appendChild(fragmentList);
+    boxSelect.appendChild(contentList);
+ };
+ const removeList = () =>{
+    const contentList = document.getElementById('listSelect');
+    contentList.remove();
+ };
+const removeItem = () => {
+    const labelRadio = document.querySelector('.select__radio:checked + label');
+
+    labelRadio.classList.add('select__item--active');
+    for(let i =0; i < palabrasDB.arrayPalabras.length; i++){
+        if(labelRadio.textContent === palabrasDB.arrayPalabras[i]){
+            palabrasDB.arrayPalabras.splice(i, 1);
+            break;
+        } 
+    }
+    localStorage.setItem('palabras', JSON.stringify(palabrasDB));
+};
 
 //Descomentando la linea de abajo puedes borrar el local storage Daniel del futuro
 // localStorage.removeItem('palabras');
-export {validar, noRepeat,addPalabra};
+export { validar, noRepeat, addPalabra, createList, removeList, removeItem };
--- Js/main.js
+++ Js/main.js
@@ -1,9 +1,11 @@
-import { validar, noRepeat, addPalabra } from "./gestorPalabras.js";
+import { validar, noRepeat, addPalabra, createList, removeList, removeItem } from "./gestorPalabras.js";
 
 const windowsFocus = document.getElementById('focus');
 const menu = document.getElementById('mainMenu');
 const configAdd = document.getElementById('formConfigAdd');
+const configRemove = document.getElementById('formConfigRemove');
 const alertConfigAdd = document.getElementById('addConfigAlert');
+
 let windowsStatus = 'index';
 
 windowsFocus.addEventListener("click", e => {
@@ -22,25 +24,27 @@ windowsFocus.addEventListener("click", e => {
     }else if((e.target.id === 'buttonE') && (windowsStatus === 'config')){
         windowsFocus.classList.add('div__config--remove');
         document.querySelector('#arrow').classList.add('arrow__config--remove');
+        createList();
         windowsStatus = "configRemove";
     }else if((e.target.id === 'buttonA') && (windowsStatus === 'config')){
         windowsFocus.classList.add('div__config--add');
         document.querySelector('#arrow').classList.add('arrow__config--add');
+        alertConfigAdd.textContent = "Lapalabra deve de tener una longitud de mm caracteres y no contener acentos";
         windowsStatus = "configAdd";
     }else if((e.target.id === 'buttonAdd') && (configAdd.textAdd.value)){ /* ! */
         if(validar(configAdd.textAdd.value)){
             alertConfigAdd.textContent = "Letra con acento detectada";
-            console.log("ERROR 1");
             return 0;
         } 
         if(noRepeat(configAdd.textAdd.value)){
             alertConfigAdd.textContent = "UPS esta palabra ya se encuentra en la lista";
-            console.log("ERROR 2");
             return 0;
         }
         alertConfigAdd.textContent = "Exelente: palabra añadida";  
         addPalabra(configAdd.textAdd.value);
         configAdd.textAdd.value = "";
+    }else if(e.target.id === 'textAdd'){
+        alertConfigAdd.textContent = "Lapalabra deve de tener una longitud de mm caracteres y no contener acentos";
     }
 });
 menu.addEventListener('click', e => {
@@ -56,10 +60,18 @@ menu.addEventListener('click', e => {
     }else if((e.target.id === 'arrow') && (windowsStatus === 'configRemove')){
         windowsFocus.classList.remove('div__config--remove');
         document.querySelector('#arrow').classList.remove('arrow__config--remove');
+        removeList();
         windowsStatus = "config";
     }else if((e.target.id === 'arrow') && (windowsStatus === 'configAdd')){
         windowsFocus.classList.remove('div__config--add');
         document.querySelector('#arrow').classList.remove('arrow__config--add');
+        alertConfigAdd.textContent = "";
         windowsStatus = "config";
     }
 });
+configRemove.addEventListener('submit', e =>{
+    e.preventDefault();
+    removeItem();
+});
+
+
